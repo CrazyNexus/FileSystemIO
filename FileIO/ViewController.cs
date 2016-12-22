69552cdd6f18e0ab4c33b4ade410dfe4924f59ff
Thomas Dubiel <td@crazynexus.de>
2016-12-22 11:07:10 +0100

    all changed files

--- FileIO/ViewController.cs
+++ FileIO/ViewController.cs
@@ -5,8 +5,9 @@
 // Copyright 2016 Thomas Dubiel. All rights reserved.
 //
 using System;
-
 using UIKit;
+using System.IO;
+
 
 namespace FileIO
 {
@@ -27,6 +28,40 @@ namespace FileIO
 		{
 			base.DidReceiveMemoryWarning();
 			// Release any cached data, images, etc that aren't in use.
+		}
+
+		partial void FileButton_TouchUpInside(UIButton sender)
+		{
+			var entries = Directory.EnumerateFiles("./");
+			foreach (var item in entries)
+			{
+				Console.WriteLine(item);
+			}
+		}
+
+		partial void ReadButton_TouchUpInside(UIButton sender)
+		{
+			var text = File.ReadAllLines("story.txt");
+			foreach (var line in text)
+			{
+				Console.WriteLine(line);
+			}
+
+		}
+
+		partial void WriteButton_TouchUpInside(UIButton sender)
+		{
+			var documents = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			Console.WriteLine(documents);
+			var filename = Path.Combine(documents, "newstory.txt");
+			Console.WriteLine(filename);
+			File.WriteAllText(filename, "This course is still awsome!");
+
+			var dirname = Path.Combine(documents, "Stories");
+			Directory.CreateDirectory(dirname);
+
+			var dirExists = Directory.Exists(dirname);
+
 		}
 	}
 }
